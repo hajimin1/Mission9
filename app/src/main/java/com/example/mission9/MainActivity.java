d70d5789b6431c11d16b30dce16da2a0cc6acb25
Jimin Ha <smjmjh26@naver.com>
2023-04-16 20:30:52 +0900

    Customer Information Input Screen Configuration

---
+++ app/src/main/java/com/example/mission9/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mission9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("고객정보 입력");
+    }
+}
